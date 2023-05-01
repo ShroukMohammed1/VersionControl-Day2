4f28937eed5452bf9bcc7dbdce60e39b4035e51b
Mahmoud Ismail <mahmoud.ism96@gmail.com>
2023-05-01 14:13:50 +0300

    Confict Test

--- HelloITI.java
+++ HelloITI.java
@@ -1,6 +1,6 @@
 ECHO is on.
 public void PrintHelloITI(){
-	System.out.println("Hello ITI");
+	System.out.println("Hello ITI Test 123");
 }
 public void PrintHelloGit(){
 	System.out.println("Hello Git");
